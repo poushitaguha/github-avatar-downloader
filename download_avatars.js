91e08361bda76876bc59b0fbdb4ee383ec637927
poushitaguha <pguha.kolkata@gmail.com>
2019-06-04 01:04:35 +0000

    Display image

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var secretRequest = require('./secrets');
 var request = require('request');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -26,3 +27,17 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   // console.log("Errors:", err);
   // console.log("Result:", result);
 });
+
+
+function downloadImageByURL(url, filePath) {
+  request.get(url, filePath)
+         .on('error', function (err) {
+          console.log(err);
+         })
+         .on('response', function (response) {
+           console.log('Response Status Code: ', response.statusCode);
+         })
+         .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
